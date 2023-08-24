93011876acb5aaa7a5ad37bce242a9e10d9ad730
yamada <shinya.yamada@benext-corp.com>
2023-08-25 07:35:45 +0900

    ✨ Add TimerCounter

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from 'react'
 import NotesList from './components/notes-list'
+import Spinner from './components/spinner'
+import TimerCounter from './components/timer-counter'
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="m-10 text-center">
       <div>
         <p>Hello World</p>
-        {/* @ts-ignore*/}
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          {/* @ts-ignore*/}
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
       </div>
     </main>
   )

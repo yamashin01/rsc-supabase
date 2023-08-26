5830c7295c15c8f10cab4a12d478d50ec6d93d18
yamada <shinya.yamada@benext-corp.com>
2023-08-26 16:27:51 +0900

    ✨ Add authentification by supabase

---
+++ app/auth/layout.tsx
@@ -0,0 +1,24 @@
+import { headers, cookies } from 'next/headers'
+import { Database } from '@/database.types'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import SupabaseListener from '../components/supabase-listener'
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+  return (
+    <>
+      <SupabaseListener accessToken={session?.access_token} />
+      {children}
+    </>
+  )
+}
---
+++ app/components/supabase-listener.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import useStore from '@/store'
+import supabase from '@/utils/supabase'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function SupabaseListener({
+  accessToken,
+}: {
+  accessToken?: string
+}) {
+  const router = useRouter()
+  const { updateLoginUser } = useStore()
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const { data } = await supabase.auth.getSession()
+      if (data.session) {
+        updateLoginUser({
+          id: data.session?.user.id,
+          email: data.session?.user.email!,
+        })
+      }
+    }
+    getUserInfo()
+    supabase.auth.onAuthStateChange((_, session) => {
+      updateLoginUser({ id: session?.user.id, email: session?.user.email! })
+      if (session?.access_token !== accessToken) {
+        router.refresh()
+      }
+    })
+  }, [accessToken, router, updateLoginUser])
+  return null
+}

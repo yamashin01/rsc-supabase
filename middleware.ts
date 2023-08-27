911dd6be703c9bd577fdeefe86e71f02597eddf1
yamada <shinya.yamada@benext-corp.com>
2023-08-28 08:13:18 +0900

    ✨ Add middleware for redirect

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareSupabaseClient({ req, res })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+  if (!session && req.nextUrl.pathname.startsWith('/auth/todo-crud')) {
+    const redirectUrl = req.nextUrl.clone()
+    redirectUrl.pathname = '/auth'
+    return NextResponse.redirect(redirectUrl)
+  }
+  return res
+}
